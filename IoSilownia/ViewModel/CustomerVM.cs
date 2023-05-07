01edde5f4da7e341c9029682108a8c9838cc53d3
Worysz <woryszlukasz@gmail.com>
2023-05-07 19:31:51 +0200

    Add project files.

---
+++ IoSilownia/ViewModel/CustomerVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IoSilownia.Model;
+
+
+namespace IoSilownia.ViewModel
+{
+    class CustomerVM:Utilities.ViewModelBase
+    {
+      private readonly PageModel _pageModel;
+        public int CustomerID
+        {
+            get { return _pageModel.CustomerCount; }
+            set { _pageModel.CustomerCount = value;OnPropertyChanged(); }
+
+
+        }
+        public CustomerVM()
+        {
+            _pageModel= new PageModel();
+            CustomerID = 100528;
+
+        }
+    }
+}
---
+++ IoSilownia/ViewModel/NavigationVM.cs
@@ -0,0 +1,42 @@
+﻿using IoSilownia.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using IoSilownia.Utilities;
+using System.Windows.Input;
+
+
+namespace IoSilownia.ViewModel
+{
+    class NavigationVM : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand HomeCommand { get; set; }
+        public ICommand CustomersCommand { get; set; }
+        public ICommand FormCommand { get; set; }
+
+
+        private void Home(object obj) => CurrentView = new HomeVM();
+        private void Customer(object obj) => CurrentView = new CustomerVM();
+
+        private void Form(object obj) => CurrentView = new FormVM();
+
+  
+        public NavigationVM()
+        {
+            HomeCommand = new RelayCommand(Home);
+            CustomersCommand = new RelayCommand(Customer);
+            FormCommand = new RelayCommand(Form);
+            CurrentView = new HomeVM();
+        }
+    }
+}
