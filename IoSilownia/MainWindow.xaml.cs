e6553a38484572bd562b965c6fcaac0f413e3f85
Worysz <woryszlukasz@gmail.com>
2023-06-08 11:39:25 +0200

    Added Clock box on the top of Navigation part.

--- IoSilownia/MainWindow.xaml.cs
+++ IoSilownia/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Windows.Navigation;
 using CMS.Ecommerce.AuthorizeNetDataContracts;
 using IoSilownia.Utilities;
+using System.Windows.Threading;
 
 namespace IoSilownia
 {
@@ -27,7 +28,19 @@ namespace IoSilownia
         {
             InitializeComponent();
 
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Aktualizowanie pola tekstowego z aktualną datą i czasem
+            txtDateTime.Text = DateTime.Now.ToString();
+        }
+
+
         private void CloseApp_Click(object sender, RoutedEventArgs e)
         {
             Close();
@@ -35,13 +48,25 @@ namespace IoSilownia
 
         private void Trener_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new Uri("/view/Customer.xaml", UriKind.Relative));
+            MainFrame.Navigate(new Uri("/view/Training.xaml", UriKind.Relative));
 
         }
 
         private void Recepcja_Click(object sender, RoutedEventArgs e)
         {
             MainFrame.Navigate(new Uri("/view/Form.xaml", UriKind.Relative));
+
+        }
+
+        private void Homes_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Uri("/view/Home.xaml", UriKind.Relative));
+
+        }
+
+        private void Customer_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Uri("/view/Customer.xaml", UriKind.Relative));
 
         }
     }
